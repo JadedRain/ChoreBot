6db52009780a318fc4a7e27f3587e3f14f1181d9
JadedRain <darkenedninja450@gmail.com>
2023-11-30 15:52:12 -0700

    Bot can now run on servers

--- ChoreBot/ChoreBot.py
+++ ChoreBot/ChoreBot.py
@@ -1,3 +1,4 @@
+import asyncio
 import discord
 import os
 from discord.ext import commands
@@ -7,3 +8,22 @@ from pclasses.guild_info import GuildInfo
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    guild_info = GuildInfo(bot.guilds)
+    await guild_info.list_guild_info()
+    
+# @bot.command(name="test")
+# async def test(ctx):
+#     await ctx.send("this bot is up and running!")
+    
+async def main():
+    await bot.start(DISCORD_TOKEN)
+    
+
+asyncio.run(main())
