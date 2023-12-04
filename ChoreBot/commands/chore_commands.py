3dbe8ac4c51d61cca9c0c6aaf2fae0a64b78ad91
JadedRain <darkenedninja450@gmail.com>
2023-12-03 22:17:13 -0700

    chores can now be marked as completed

--- ChoreBot/commands/chore_commands.py
+++ ChoreBot/commands/chore_commands.py
@@ -23,12 +23,19 @@ class ChoreCommands(commands.Cog):
     @commands.command(name="shchores")
     async def show_chores(self, ctx):
         for i in range(len(self.chore_list)):
-            await ctx.channel.send(f"Chore: {self.chore_list[i].get_chore()} Person: <@{self.chore_list[i].get_person()}>")
+            await ctx.channel.send(f"Chore: {self.chore_list[i].get_chore()} Person: <@{self.chore_list[i].get_person()}> Completed: {self.chore_list[i].completed}")
             
     @commands.command(name="assign")
     async def assign_chores(self, ctx):
         for chore in self.chore_list:
             chore.set_person(random.choice(list(self.person_list.values())))
+            
+    @commands.command(name="complete")
+    async def complete_chore(self, ctx, *chore):
+        for c in self.chore_list:
+            if c.get_chore() == ' '.join(chore[:]).title() and ctx.author.id == c.get_person():
+                c.completed = True
+            
 
             
     
